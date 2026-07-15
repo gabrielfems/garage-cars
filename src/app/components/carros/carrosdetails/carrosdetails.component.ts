20c355d23681abff81a826ae3e930e935d33d1b1
devgabs1 <gabriel.fe132@gmail.com>
2026-07-14 22:07:30 -0300

    carros: implementar formulário de detalhes e salvar com navegação

--- src/app/components/carros/carrosdetails/carrosdetails.component.ts
+++ src/app/components/carros/carrosdetails/carrosdetails.component.ts
@@ -1,12 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Carro } from 'app/models/carro';
+import { MdbFormsModule } from 'mdb-angular-ui-kit/forms'
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-carrosdetails',
   standalone: true,
-  imports: [],
+  imports: [MdbFormsModule, FormsModule],
   templateUrl: './carrosdetails.component.html',
   styleUrl: './carrosdetails.component.scss'
 })
 export class CarrosdetailsComponent {
 
+  carro: Carro = new Carro(0, "");
+  router = inject(ActivatedRoute);
+  router2 = inject(Router);
+
+
+
+  constructor() {
+    let id = this.router.snapshot.params['id'];
+    if (id > 0) {
+      this.findById(id);
+    }
+  }
+
+  findById(id: number) {
+    //Simulação de busca de carro por id
+    let carroRetornado: Carro = new Carro(id, "Golf");
+    this.carro = carroRetornado;
+  }
+
+  save(){
+    if(this.carro.id > 0){
+      Swal.fire({
+      title: 'Editado com sucesso!!',
+      icon: 'success',
+      confirmButtonText: 'Ok'
+    })
+      this.router2.navigate(['/admin/carros'], { state: { carroEditado: this.carro } });
+
+    } else {   
+      Swal.fire({
+      title: 'Salvo com sucesso!!',
+      icon: 'success',
+      confirmButtonText: 'Ok'
+    })
+        this.router2.navigate(['/admin/carros'], { state: { carroNovo: this.carro } });
+    }
+  }
 }
