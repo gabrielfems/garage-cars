f56c477318a3ac0de7acdc0810df8e473b10a260
devgabs1 <gabriel.fe132@gmail.com>
2026-07-14 22:07:30 -0300

    login: adicionar formulário e validação simples de autenticação

--- src/app/components/layout/login/login.component.ts
+++ src/app/components/layout/login/login.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
+import { inject } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MdbFormsModule],
+  imports: [MdbFormsModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
 
+  usuario!: string;
+  senha!: string;
+
+  router = inject(Router);
+
+  logar() {
+    if (this.usuario === 'admin' && this.senha === 'admin') {
+      this.router.navigate(['admin/carros']);
+    } else 
+      alert('Credenciais inválidas. Tente novamente.');
+}
 }
