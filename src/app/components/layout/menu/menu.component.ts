0ce536bc950ba2b8f145a9f37a539db032be4392
devgabs1 <gabriel.fe132@gmail.com>
2026-07-14 22:07:30 -0300

    menu: criar barra de navegação e importar módulo de collapse

--- src/app/components/layout/menu/menu.component.ts
+++ src/app/components/layout/menu/menu.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MdbCollapseModule } from 'mdb-angular-ui-kit/collapse';  
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [],
+  imports: [MdbCollapseModule],
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
